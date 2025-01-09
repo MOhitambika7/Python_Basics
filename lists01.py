94493c21586ebb29f73af6f0bda6d983c4850eb0
MOhitambika7 <mohitambikanand7@gmail.com>
2025-01-09 10:56:14 +0545

    Some types how can we know the structure of lists.

---
+++ lists01.py
@@ -0,0 +1,16 @@
+#Lists ----> Mutable, Ordered collection of items. Can hold multiple data types.
+#Creating a list.
+
+Guitars = [] # Is an empty list.
+
+nameGuitars = ["Givson", "Fender", "Ibanez", "Kadence"] #List of string data type.
+print(nameGuitars)
+
+listInteger = [1, 2, 3, 4, 5] #List of integer data type(i.e int).
+print(listInteger)
+
+listMixed = [1, "Hey There", 3.5, True] #List of integer, string, float, bool data type.
+print(listMixed)
+
+listNested = [[1,2], [3,4]] #Nested list.
+print(listNested)
